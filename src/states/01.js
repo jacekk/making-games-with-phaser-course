6a0f79b0bd7b289a123aeed676734100f0ff83ef
jacekk <gitkraken@jacek-k.net>
2018-01-01 02:51:58 +0100

    used tilemap in state01;

--- src/states/01.js
+++ src/states/01.js
@@ -11,10 +11,26 @@ state.prototype = {
 
 export default state
 
-function preload() {}
+function preload() {
+    const fieldPath = '/assets/tilemaps/field.json'
+    const grassPath = '/assets/tilemaps/grassTiles.png'
+    const rockPath = '/assets/tilemaps/rockTiles.png'
+
+    game.load.tilemap('field', fieldPath, null, Phaser.Tilemap.TILED_JSON)
+    game.load.image('grassTiles', grassPath)
+    game.load.image('rockTiles', rockPath)
+}
 
 function create() {
     bindStatesChangeKeys()
+
+    const map = game.add.tilemap('field')
+
+    map.addTilesetImage('grassTiles')
+    map.addTilesetImage('rockTiles')
+
+    const grass = map.createLayer('grass')
+    const rocks = map.createLayer('rocks')
 }
 
 function update() {}
